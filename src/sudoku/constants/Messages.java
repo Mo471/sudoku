709038ee9db904ee81e0f38735bbe275ea15b0fd
Mukhammad <mukhammad9292@gmail.com>
2022-02-23 03:10:16 +0000

    Commiting full project

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,8 @@
+package sudoku.constants;
+
+public class Messages {
+
+    public static final String GAME_COMPLETE = "Congratulations, you won! Do you wnat to launch a new game?";
+    public static final String ERROR = "ERROR! :(";
+
+}
